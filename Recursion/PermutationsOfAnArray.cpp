3ecca5964d3ff15f31ff9f7420f78c011d3700e5
Prapti <121173640+praaptiiiii@users.noreply.github.com>
2024-07-03 20:33:46 +0530

    Update PermutationsOfAnArray.cpp

--- Recursion/PermutationsOfAnArray.cpp
+++ Recursion/PermutationsOfAnArray.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include<vector>
 #include<algorithm>
@@ -26,6 +27,39 @@ int main()
     vector<vector<int>> ans;
     int freq[arr.size()]={0};
     perm(arr,ans,temp,freq);
+    
+    for (auto vec: ans){
+        cout<<"{";
+        for (auto item: vec){
+            cout<<" "<<item<<" ";
+        }
+        cout<<"}";
+    }
+    return 0;
+}
+*/
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void perm(vector<int> &arr, vector<vector<int>> &ans,int index){
+    if (index==arr.size()){
+        ans.push_back(arr);
+        return;
+    }
+    for (int i=index; i<arr.size(); i++){
+        swap(arr[index],arr[i]);
+        perm(arr,ans,index+1);
+        swap(arr[index],arr[i]);
+    }
+}
+
+int main()
+{
+    vector<int> arr={1,5,3};
+    vector<vector<int>> ans;
+    perm(arr,ans,0);
     
     for (auto vec: ans){
         cout<<"{";
