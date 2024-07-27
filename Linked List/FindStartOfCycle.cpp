662f78b52b6e64fa1703cbffeb1527d94ea44e97
Prapti <121173640+praaptiiiii@users.noreply.github.com>
2024-07-28 00:04:23 +0530

    Create FindStartOfCycle.cpp

---
+++ Linked List/FindStartOfCycle.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (head == NULL || head->next == NULL) {
+            return NULL;
+        }
+        
+        ListNode *slow = head->next;
+        ListNode *fast = head->next->next;
+        
+        while (fast != NULL && fast->next != NULL) {
+            if (slow == fast) {
+                break;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        if (fast == NULL || fast->next == NULL) {
+            return NULL;
+        }
+        
+        slow = head;
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        return slow;
+    }
+};
